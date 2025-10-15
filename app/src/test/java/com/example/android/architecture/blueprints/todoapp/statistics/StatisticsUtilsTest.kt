dd37bdfb9fc1b9e7ddac6a90597012e20e1ee731
cherry-0 <susanpark0@snu.ac.kr>
2025-10-15 18:01:24 +0900

    skeleton code for 7-2 class

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,11 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest {
+    @Test
+    fun getActiveAndCompletedStats() {
+    }
+
+}
