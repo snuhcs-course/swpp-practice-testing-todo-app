fde565b9fefc36d6ba087e1c68135c08d62d28a9
Hyunsoo Kim <hyunsoo.kim@hcs.snu.ac.kr>
2025-10-23 11:16:55 +0900

    Base project with Views - NO testing code

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.5" apply false
+}
